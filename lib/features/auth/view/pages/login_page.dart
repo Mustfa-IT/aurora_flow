fcdaaccc6e2d937172ee2862dd1c6b5690b4bc91
ssaram512 <133879993+ssaram512@users.noreply.github.com>
2025-02-21 16:23:44 +0300

    Add Forgot Password functionality with email input and verification flow

--- lib/features/auth/view/pages/login_page.dart
+++ lib/features/auth/view/pages/login_page.dart
@@ -21,6 +21,7 @@ library;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_app/features/auth/view/pages/ForgotPassword_Page.dart';
 import 'package:task_app/features/auth/view/pages/register_page.dart';
 import '../bloc/auth_bloc.dart';
 
@@ -202,12 +203,14 @@ class _LoginPageState extends State<LoginPage> {
                             TextButton(
                               // منطق استعادة كلمة المرور إن وُجد
                               onPressed: () {
-                                AppUtils.showComingSoonMessage(
-                                    context); // استدعاء الدالة
+                                     Navigator.of(context).push(
+                                       MaterialPageRoute(builder: (context) => ForgotPasswordPage()),
+                                                );
                               },
                               child: const Text(
                                 'I forgot my password',
                                 style: TextStyle(color: Colors.blue),
+                                
                               ),
                             ),
                           ],
